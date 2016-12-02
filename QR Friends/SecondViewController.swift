f5500991a3b34e49f74123f7c50c4f48a2de34fa
Roman Zhikharevich <rzhikharevich@gmail.com>
2016-12-02 16:30:51 +0300

    Initial Commit

---
+++ QR Friends/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QR Friends
+//
+//  Created by Roman Zhikharevich on 02.12.16.
+//  Copyright © 2016 Roman Zhikharevich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
